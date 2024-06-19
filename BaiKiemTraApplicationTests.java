8281fc321208f84a5ed3dc13f417437c3c13998f
Matchadendakhongduong <152264193+Matchadendakhongduong@users.noreply.github.com>
2024-06-20 00:51:19 +0700

    Add files via upload

---
+++ BaiKiemTraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Buoi8.BaiKiemTra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiKiemTraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
